f72a982799f7876c102cf1e672454dbfa2873855
matos-hayrapetyan <matoshayrapetyan@gmail.com>
2016-11-18 20:17:07 +0400

    global update

---
+++ templates/admin/licensing.php
@@ -0,0 +1,16 @@
+<div style="width:95%">
+    <p>
+        <?php _e( 'You are using the Lite version of the Google Maps for WordPress. If you want to get more awesome options,
+        advanced features, settings to customize every area of the plugin, then check out the Full License plugin. The
+        full version of the plugin is available in 3 different packages of one-time payment.', 'hugeit_maps' ); ?>
+    </p>
+    <br/><br/>
+    <a href="http://huge-it.com/google-map/" class="button-primary" target="_blank"><?php _e( 'Purchase a License', 'hugeit_maps' ); ?></a>
+    <br/><br/><br/>
+    <p><?php _e( 'After the purchasing the full version follow this steps:', 'hugeit_maps' ); ?></p>
+    <ol>
+        <li><?php _e( 'Deactivate Huge-IT Google Maps Plugin', 'hugeit_maps' ); ?></li>
+        <li><?php _e( 'Delete Huge-IT Google Maps Plugin', 'hugeit_maps' ); ?></li>
+        <li><?php _e( 'Install the downloaded full version version of the plugin', 'hugeit_maps' ); ?></li>
+    </ol>
+</div>
