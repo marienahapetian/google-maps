b45eeb439a96042ebb2952aad88f9f8ad9be6a83
marienahapetian <marie.nahapetian@gmail.com>
2017-01-17 17:45:14 +0400

    featured plugins translation added

--- templates/admin/featured-plugins.php
+++ templates/admin/featured-plugins.php
@@ -5,142 +5,127 @@
 
 ?>
 
-<?php
-/**
- * Template for featured plugins page
- */
-
-?>
-
 
 <div class="huge-plugins">
 	<div class="plaings-block">
 		<div class="plugin">
 			<div class="image-block photo-gallery"><a href="http://huge-it.com/wordpress-photo-gallery/" target="_blank"></a></div>
 			<div class="info-block">
-				<h2><a href="http://huge-it.com/wordpress-photo-gallery/" target="_blank">Photo Gallery </a></h2>
-				<p>Access advanced options and customization tools of this ultimate gallery, use the best ever functionality and unique features.</p>
-				<a href="http://huge-it.com/wordpress-photo-gallery/" class="view" target="_blank">View Plugin</a>
+				<h2><a href="http://huge-it.com/wordpress-photo-gallery/" target="_blank"><?php _e('Photo Gallery', 'hugeit-maps'); ?> </a></h2>
+				<p><?php _e('Access advanced options and customization tools of this ultimate gallery, use the best ever functionality and unique features.', 'hugeit-maps'); ?></p>
+				<a href="http://huge-it.com/wordpress-photo-gallery/" class="view" target="_blank"><?php _e('View Plugin', 'hugeit-maps'); ?></a>
 			</div>
 		</div>
 		<div class="plugin">
 			<div class="image-block portfolio-gallery"><a href="http://huge-it.com/portfolio-gallery/" target="_blank"></a></div>
 			<div class="info-block">
-				<h2><a href="http://huge-it.com/portfolio-gallery/" target="_blank">Portfolio Gallery</a></h2>
-				<p>Best Portfolio plugin for WordPress with 9 unique views, lightbox options, slideshows, advanced customization solutions.</p>
-				<a href="http://huge-it.com/portfolio-gallery/" class="view" target="_blank">View Plugin</a>
+				<h2><a href="http://huge-it.com/portfolio-gallery/" target="_blank"><?php _e('Portfolio Gallery', 'hugeit-maps'); ?></a></h2>
+				<p><?php _e('Best Portfolio plugin for WordPress with 9 unique views, lightbox options, slideshows, advanced customization solutions.', 'hugeit-maps'); ?></p>
+				<a href="http://huge-it.com/portfolio-gallery/" class="view" target="_blank"><?php _e('View Plugin', 'hugeit-maps'); ?></a>
 			</div>
 		</div>
 		<div class="plugin">
 			<div class="image-block responsive-slider"><a href="http://huge-it.com/wordpress-responsive-slider/" target="_blank"></a></div>
 			<div class="info-block">
-				<h2><a href="http://huge-it.com/wordpress-responsive-slider/" target="_blank">Responsive Slider</a></h2>
-				<p>Make stunning sliders using advanced video, YouTube, Vimeo options. Link slides with URL, add text layers, animations and more.</p>
-				<a href="http://huge-it.com/wordpress-responsive-slider/" class="view" target="_blank">View Plugin</a>
+				<h2><a href="http://huge-it.com/wordpress-responsive-slider/" target="_blank"><?php _e('Responsive Slider', 'hugeit-maps'); ?></a></h2>
+				<p><?php _e('Make stunning sliders using advanced video, YouTube, Vimeo options. Link slides with URL, add text layers, animations and more.', 'hugeit-maps'); ?></p>
+				<a href="http://huge-it.com/wordpress-responsive-slider/" class="view" target="_blank"><?php _e('View Plugin', 'hugeit-maps'); ?></a>
 			</div>
 		</div>
 		<div class="plugin">
 			<div class="image-block forms"><a href="http://huge-it.com/forms/" target="_blank"></a></div>
 			<div class="info-block">
-				<h2><a href="http://huge-it.com/forms/" target="_blank">Forms</a></h2>
-				<p>Create different recipients for each form, send newsletters, use ready-to-go fields and more with Forms Pro.</p>
-				<a href="http://huge-it.com/forms/" class="view" target="_blank">View Plugin</a>
+				<h2><a href="http://huge-it.com/forms/" target="_blank"><?php _e('Forms', 'hugeit-maps'); ?></a></h2>
+				<p><?php _e('Create different recipients for each form, send newsletters, use ready-to-go fields and more with Forms Pro.', 'hugeit-maps'); ?></p>
+				<a href="http://huge-it.com/forms/" class="view" target="_blank"><?php _e('View Plugin', 'hugeit-maps'); ?></a>
 			</div>
 		</div>
 		<div class="plugin">
 			<div class="image-block gallery"><a href="http://huge-it.com/wordpress-gallery/" target="_blank"></a></div>
 			<div class="info-block">
-				<h2><a href="http://huge-it.com/wordpress-gallery/" target="_blank">Gallery</a></h2>
-				<p>Create unlimited stunning galleries of images and videos,  using 8 flexible and customizable views.Show them in a simple gallery forms.</p>
-				<a href="http://huge-it.com/wordpress-gallery/" class="view" target="_blank">View Plugin</a>
+				<h2><a href="http://huge-it.com/wordpress-gallery/" target="_blank"><?php _e('Gallery', 'hugeit-maps'); ?></a></h2>
+				<p><?php _e('Create unlimited stunning galleries of images and videos,  using 8 flexible and customizable views.Show them in a simple gallery forms.', 'hugeit-maps'); ?></p>
+				<a href="http://huge-it.com/wordpress-gallery/" class="view" target="_blank"><?php _e('View Plugin', 'hugeit-maps'); ?></a>
 			</div>
 		</div>
 		<div class="plugin">
 			<div class="image-block slider"><a href="http://huge-it.com/slider/" target="_blank"></a></div>
 			<div class="info-block">
-				<h2><a href="http://huge-it.com/slider/" target="_blank">Slider</a></h2>
-				<p>Use million times downloaded slider with theme editor, post feed sliders,video support and advanced options right away.</p>
-				<a href="http://huge-it.com/slider/" class="view" target="_blank">View Plugin</a>
-			</div>
-		</div>
-		<div class="plugin">
-			<div class="image-block google-map"><a href="http://huge-it.com/google-map/" target="_blank"></a></div>
-			<div class="info-block">
-				<h2><a href="http://huge-it.com/google-map/" target="_blank">Google map</a></h2>
-				<p>Make advanced customization, add directions and layers, export maps in CSV file, add links and HTML with Google Maps Pro.</p>
-				<a href="http://huge-it.com/google-map/" class="view" target="_blank">View Plugin</a>
+				<h2><a href="http://huge-it.com/slider/" target="_blank"><?php _e('Slider', 'hugeit-maps'); ?></a></h2>
+				<p><?php _e('Use million times downloaded slider with theme editor, post feed sliders,video support and advanced options right away.', 'hugeit-maps'); ?></p>
+				<a href="http://huge-it.com/slider/" class="view" target="_blank"><?php _e('View Plugin', 'hugeit-maps'); ?></a>
 			</div>
 		</div>
 		<div class="plugin">
 			<div class="image-block video-gallery"><a href="http://huge-it.com/wordpress-video-gallery/" target="_blank"></a></div>
 			<div class="info-block">
-				<h2><a href="http://huge-it.com/wordpress-video-gallery/" target="_blank">Video Gallery</a></h2>
-				<p>Create the best video galleries with this plugin using 7 views, design and lightbox customization, advanced options and more.</p>
-				<a href="http://huge-it.com/wordpress-video-gallery/" class="view" target="_blank">View Plugin</a>
+				<h2><a href="http://huge-it.com/wordpress-video-gallery/" target="_blank"><?php _e('Video Gallery', 'hugeit-maps'); ?></a></h2>
+				<p><?php _e('Create the best video galleries with this plugin using 7 views, design and lightbox customization, advanced options and more.', 'hugeit-maps'); ?></p>
+				<a href="http://huge-it.com/wordpress-video-gallery/" class="view" target="_blank"><?php _e('View Plugin', 'hugeit-maps'); ?></a>
 			</div>
 		</div>
 		<div class="plugin">
 			<div class="image-block login"><a href="http://huge-it.com/wordpress-login/" target="_blank"></a></div>
 			<div class="info-block">
-				<h2><a href="http://huge-it.com/wordpress-login/" target="_blank">Login</a></h2>
-				<p>Add Login and sign up buttons on your site with amazing popup. Customize and style it to your liking, using security options and reCAPTCHA.</p>
-				<a href="http://huge-it.com/wordpress-login/" class="view" target="_blank">View Plugin</a>
+				<h2><a href="http://huge-it.com/wordpress-login/" target="_blank"><?php _e('Login', 'hugeit-maps'); ?></a></h2>
+				<p><?php _e('Add Login and sign up buttons on your site with amazing popup. Customize and style it to your liking, using security options and reCAPTCHA.', 'hugeit-maps'); ?></p>
+				<a href="http://huge-it.com/wordpress-login/" class="view" target="_blank"><?php _e('View Plugin', 'hugeit-maps'); ?></a>
 			</div>
 		</div>
 		<div class="plugin">
 			<div class="image-block catalog"><a href="http://huge-it.com/product-catalog/" target="_blank"></a></div>
 			<div class="info-block">
-				<h2><a href="http://huge-it.com/product-catalog/" target="_blank">Catalog</a></h2>
-				<p>Your ultimate store and catalog in 5 stylish views. Create products, design, customize categories, product pages, messages, comments, ratings. </p>
-				<a href="http://huge-it.com/product-catalog/" class="view" target="_blank">View Plugin</a>
+				<h2><a href="http://huge-it.com/product-catalog/" target="_blank"><?php _e('Catalog', 'hugeit-maps'); ?></a></h2>
+				<p><?php _e('Your ultimate store and catalog in 5 stylish views. Create products, design, customize categories, product pages, messages, comments, ratings.', 'hugeit-maps'); ?> </p>
+				<a href="http://huge-it.com/product-catalog/" class="view" target="_blank"><?php _e('View Plugin', 'hugeit-maps'); ?></a>
 			</div>
 		</div>
 		<div class="plugin">
 			<div class="image-block lightbox"><a href="http://huge-it.com/lightbox/" target="_blank"></a></div>
 			<div class="info-block">
-				<h2><a href="http://huge-it.com/lightbox/" target="_blank">Lightbox</a></h2>
-				<p>Pop up your images and videos in beautiful lightbox, share on social media and secure your images with watermark.</p>
-				<a href="http://huge-it.com/lightbox/" class="view" target="_blank">View Plugin</a>
+				<h2><a href="http://huge-it.com/lightbox/" target="_blank"><?php _e('Lightbox', 'hugeit-maps'); ?></a></h2>
+				<p><?php _e('Pop up your images and videos in beautiful lightbox, share on social media and secure your images with watermark.', 'hugeit-maps'); ?></p>
+				<a href="http://huge-it.com/lightbox/" class="view" target="_blank"><?php _e('View Plugin', 'hugeit-maps'); ?></a>
 			</div>
 		</div>
 		<div class="plugin">
 			<div class="image-block video-player"><a href="http://huge-it.com/video-player/" target="_blank"></a></div>
 			<div class="info-block">
-				<h2><a href="http://huge-it.com/video-player/" target="_blank">Video Player</a></h2>
-				<p>Create and customize your video player using advanced options, select skins and themes for your player.</p>
-				<a href="http://huge-it.com/video-player/" class="view" target="_blank">View Plugin</a>
+				<h2><a href="http://huge-it.com/video-player/" target="_blank"><?php _e('Video Player', 'hugeit-maps'); ?></a></h2>
+				<p><?php _e('Create and customize your video player using advanced options, select skins and themes for your player.', 'hugeit-maps'); ?></p>
+				<a href="http://huge-it.com/video-player/" class="view" target="_blank"><?php _e('View Plugin', 'hugeit-maps'); ?></a>
 			</div>
 		</div>
 		<div class="plugin">
 			<div class="image-block princing-table"><a href="http://huge-it.com/wordpress-pricing-table-builder/" target="_blank"></a></div>
 			<div class="info-block">
-				<h2><a href="http://huge-it.com/wordpress-pricing-table-builder/" target="_blank">Princing Table</a></h2>
-				<p>Sell your products and services using 120 stunning pricing table themes and use more advanced options to design them.</p>
-				<a href="http://huge-it.com/wordpress-pricing-table-builder/" class="view" target="_blank">View Plugin</a>
+				<h2><a href="http://huge-it.com/wordpress-pricing-table-builder/" target="_blank"><?php _e('Princing Table', 'hugeit-maps'); ?></a></h2>
+				<p><?php _e('Sell your products and services using 120 stunning pricing table themes and use more advanced options to design them.', 'hugeit-maps'); ?></p>
+				<a href="http://huge-it.com/wordpress-pricing-table-builder/" class="view" target="_blank"><?php _e('View Plugin', 'hugeit-maps'); ?></a>
 			</div>
 		</div>
 		<div class="plugin">
 			<div class="image-block colorbox"><a href="http://huge-it.com/colorbox/" target="_blank"></a></div>
 			<div class="info-block">
-				<h2><a href="http://huge-it.com/colorbox/" target="_blank">Colorbox</a></h2>
-				<p>Add advanced solutions for your Colorbox plugin using customization settings and bring best of popup and lightbox effects.</p>
-				<a href="http://huge-it.com/colorbox/" class="view" target="_blank">View Plugin</a>
+				<h2><a href="http://huge-it.com/colorbox/" target="_blank"><?php _e('Colorbox', 'hugeit-maps'); ?></a></h2>
+				<p><?php _e('Add advanced solutions for your Colorbox plugin using customization settings and bring best of popup and lightbox effects.', 'hugeit-maps'); ?></p>
+				<a href="http://huge-it.com/colorbox/" class="view" target="_blank"><?php _e('View Plugin', 'hugeit-maps'); ?></a>
 			</div>
 		</div>
 		<div class="plugin">
 			<div class="image-block share-buttons"><a href="http://huge-it.com/share-buttons/" target="_blank"></a></div>
 			<div class="info-block">
-				<h2><a href="http://huge-it.com/share-buttons/" target="_blank">Share buttons</a></h2>
-				<p>Spread the message of your websites all around social media using 11 beautiful design options and style settings of Share Buttons.</p>
-				<a href="http://huge-it.com/share-buttons/" class="view" target="_blank">View Plugin</a>
+				<h2><a href="http://huge-it.com/share-buttons/" target="_blank"><?php _e('Share buttons', 'hugeit-maps'); ?></a></h2>
+				<p><?php _e('Spread the message of your websites all around social media using 11 beautiful design options and style settings of Share Buttons.', 'hugeit-maps'); ?></p>
+				<a href="http://huge-it.com/share-buttons/" class="view" target="_blank"><?php _e('View Plugin', 'hugeit-maps'); ?></a>
 			</div>
 		</div>
 		<div class="plugin">
 			<div class="image-block builder"><a href="http://huge-it.com/wordpress-price-table-builder" target="_blank"></a></div>
 			<div class="info-block">
-				<h2><a href="http://huge-it.com/wordpress-price-table-builder" target="_blank">Price Table Builder</a></h2>
-				<p>Access full customization list of Pricing Table Builder and change every corner of it, by customizing to your needs.</p>
-				<a href="http://huge-it.com/wordpress-price-table-builder" class="view" target="_blank">View Plugin</a>
+				<h2><a href="http://huge-it.com/wordpress-price-table-builder" target="_blank"><?php _e('Price Table Builder', 'hugeit-maps'); ?></a></h2>
+				<p><?php _e('Access full customization list of Pricing Table Builder and change every corner of it, by customizing to your needs.', 'hugeit-maps'); ?></p>
+				<a href="http://huge-it.com/wordpress-price-table-builder" class="view" target="_blank"><?php _e('View Plugin', 'hugeit-maps'); ?></a>
 			</div>
 		</div>
 	</div>
