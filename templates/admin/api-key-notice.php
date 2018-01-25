a3538a8aaac8664d2a6e4eb24b28581df8df19dd
marie-grandwp <marie.nahapetian@gmail.com>
2018-01-25 13:18:21 +0400

    api key notice translated

--- templates/admin/api-key-notice.php
+++ templates/admin/api-key-notice.php
@@ -7,18 +7,18 @@
 <div id="hg_gmaps_no_api_key_big_notice" class="error">
 	<p class="hg_mui_heading"><?php _e( 'Attention!', 'hg_gmaps' ); ?></br><?php _e( 'Before you begin using Google Map plugin, please note that All Google Maps users now required to have an API key to function. You can read more about that', 'hg_gmaps' ); ?> <a href="https://googlegeodevelopers.blogspot.am/2016/06/building-for-scale-updates-to-google.html" target="_blank"><?php _e( 'here.', 'hg_gmaps' ); ?></a></p>
 	<div><a class="hg_mui_btn hg_mui_btn_raised_blue" target="_blank" href="https://console.developers.google.com/flows/enableapi?apiid=maps_backend,geocoding_backend,directions_backend,distance_matrix_backend,elevation_backend,places_backend&keyType=CLIENT_SIDE&reusekey=true"><?php _e( 'Register for Google Maps API now', 'hg_gmaps' ); ?></a></div>
-	<p class="hg_mui_heading">Once registered, simply paste your API key here and press the save button. It will activate in 5-10 minutes.</p>
+	<p class="hg_mui_heading"><?php _e('Once registered, simply paste your API key here and press the save button. It will activate in 5-10 minutes.','hg_gmaps');?></p>
 	<div>
 		<form action="" method="post">
 			<label class="hg_mui_text">
-				<span class="hg_mui_label mui_label_mt11">API KEY</span>
+				<span class="hg_mui_label mui_label_mt11"><?php _e('API KEY','hg_gmaps');?></span>
 				<div class="hg_mui_input_block">
-					<input name="hg_gmaps_api_key_input" class="hg_gmaps_api_key_input" value="" required="required" type="text"><span class="control_title">Input the api key here</span>
+					<input name="hg_gmaps_api_key_input" class="hg_gmaps_api_key_input" value="" required="required" type="text"><span class="control_title"><?php _e('Input the api key here','hg_gmaps');?></span>
 					<div class="hg_mui_bar"></div>
 				</div>
 			</label>
-			<div class="hg_gmaps_apply_action"><button class="hg_gmaps_save_api_key_button hg_mui_btn hg_mui_btn_raised_green">Save</button><span class="spinner"></span></div>
+			<div class="hg_gmaps_apply_action"><button class="hg_gmaps_save_api_key_button hg_mui_btn hg_mui_btn_raised_green"><?php _e('Save','hg_gmaps');?></button><span class="spinner"></span></div>
 		</form>
 	</div>
-	<p class="hg_mui_heading">Need help? <a href="http://huge-it.com/contact-us/" target="_blank">Contact Us</a> and we will help you with installation.</p>
+	<p class="hg_mui_heading"><?php _e('Need help?','hg_gmaps');?> <a href="http://huge-it.com/contact-us/" target="_blank"><?php _e('Contact Us','hg_gmaps');?></a> <?php _e('and we will help you with installation.','hg_gmaps');?></p>
 </div>
